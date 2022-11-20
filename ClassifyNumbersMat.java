50900b713e83c8033a767b39db14309d1f99fa2c
Avocadoobie <117540479+Avocadoobie@users.noreply.github.com>
2022-11-20 04:01:46 -0500

    Edit1

--- ClassifyNumbersMat.java
+++ ClassifyNumbersMat.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 import java.util.Scanner;
 
 public class ClassifyNumbersMat {
@@ -13,35 +13,31 @@ public class ClassifyNumbersMat {
         Scanner sc = new Scanner(System.in);
         int answer = sc.nextInt();
         array = new int[answer];
-
+        
         System.out.println("Enter positive or negative integers elements, including 0 :");
-
         for (int i = 0; i < answer; i++) {
             array[i] = sc.nextInt();
         }
-        System.out.println(Arrays.toString(array));
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
-                even++;
-            }
-        }
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 1) {
-                odd++;
-            }
-        }
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) {
+        for (int i = 0; i < array.length; i++) 
+        { System.out.printf(array[i] + " ");
+            if (array[i] == 0) 
+            {
                 zero++;
+                continue;
+            }
+            if (array[i] % 2 == 1) 
+            {
+                odd++;
+                continue;
+            }
+            if (array[i]%2 != 1) 
+            {
+                even++;
             }
         }
         System.out.println("the amount of even numbers is: " + even);
         System.out.println("the amount of odd numbers is: " + odd);
         System.out.println("the amount of zeros are: " + zero);
-
-        sc.close();
     }
-
-}
+}    
 
